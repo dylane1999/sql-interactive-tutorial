4bcf2595e19d2b0b8e140faf586e689ea128f896
Dylan Edwards <dylanedwards290@gmail.com>
2021-01-03 00:01:14 -0800

    add route to get author info from simplecast

--- server/routes/route1.js
+++ server/routes/route1.js
@@ -1,9 +1,20 @@
 module.exports = (app) => {
-  app.get("/route1/test", async (req, res) => {
+  const axios = require("axios");
+
+  app.get("/getAuthorData", async (req, res) => {
     try {
-      res.send({ test: "jsonObj" });
-    } catch (err) {
-      res.send(404);
+      const simplecastRequest = await axios.get(
+        "https://api.simplecast.com/authors/eb5d2dd2-3fd4-441d-b640-871b7893a856",
+        {
+          headers: {
+            Token:
+              "eyJhcGlfa2V5IjoiNzVkMzc3N2M3NWFhM2QwOTkxOWEyZTI4ZjhiM2M1YTkifQ==",
+          },
+        }
+      );
+      res.send({"simplecastData" : simplecastRequest.data});
+    } catch (error) {
+      res.status(400).json({"error": error});
     }
   });
 };
