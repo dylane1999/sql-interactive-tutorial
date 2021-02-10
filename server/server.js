26d1f859718b5d4a347981997c2f4fdef8dbbb9e
Dylan Morison <dylan@g.morison.org>
2021-02-10 12:15:22 -0800

    Serverside changes to routing and importing

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 import express from "express";
-import cors from "cors"
-import dotenv from "dotenv"
+import cors from "cors";
+import dotenv from "dotenv";
+import episodeRouter from "./routes/episodeRoutes.js";
 // const bodyParser = require('body-parser');
 
-const PORT = config.get("PORT")
+const PORT = config.get("PORT");
 
 const app = express();
 app.use(cors());
@@ -11,11 +12,13 @@ app.use(express.json());
 // app.use(bodyParser);
 app.use(express.urlencoded({ extended: false }));
 
+app.use(episodeRouter);
+
 require("./routes/episodeRoutes")(app);
 require("./routes/route2.js")(app);
 
 var port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-	console.log('server started on port 5000');
+	console.log("server started on port 5000");
 });
