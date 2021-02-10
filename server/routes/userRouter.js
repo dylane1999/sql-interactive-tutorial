94c05fc8243f589f8a1da9241f3d0c09dd3e2eb1
Dylan Morison <dylan@g.morison.org>
2021-02-10 15:19:37 -0800

    added basic users query to get mailingList

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -6,6 +6,15 @@ const User = mongoose.model("User");
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+	await User.find((err, docs) => {
+		if (err) res.status(204).send("No users found");
+
+		console.log(docs);
+		res.status(200).send(docs);
+	});
+});
+
 router.post("/signup", async (req, res) => {
 	const { email, username } = req.body;
 	const user = new User({ email, username });
