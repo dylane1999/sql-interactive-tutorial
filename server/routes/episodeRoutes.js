cce513a4b448b8f8d523f19fdd4f17ebcf2150ef
dan murphy <danielmurph8@gmail.com>
2021-02-14 19:28:32 -0500

    remove analytics routes from episodeRoutes.js

--- server/routes/episodeRoutes.js
+++ server/routes/episodeRoutes.js
@@ -56,35 +56,6 @@ router.get("/:episodeId", async (req, res) => {
   }
 });
 
-router.get("/analytics/top_10", async (req, res) => {
-  /**
-   * Gets the top 10 episodes for a specific podcast (based off ``podcastId``).
-   *
-   * @param req -> the request object which contains the body and params (``podcastId``) of the request
-   * @param res -> the response object, which is returned with the correct status header and top ten episodes data
-   *
-   * @return -> res.status(200).send(episodeData) || res.status(400).json(error)
-   */
-  try {
-    const podcastId = config.get("PODCAST_ID");
-    const topEpisodeData = await axios.get(
-      `https://api.simplecast.com/analytics/episodes/top_10?podcast=${podcastId}`,
-      {
-        headers: {
-          authorization: SIMPLECAST_BEARER,
-        },
-      }
-    );
-	console.log(topEpisodeData);
-    res.status(200).send(topEpisodeData.data);
-  } catch (error) {
-	  console.log(error);
-	  res.status(400).json({ error: error });
-  }
-});
-
-export default router;
-
 //https://api.simplecast.com/podcasts/5b3564ed-5910-4943-a8f8-1cd845425e53/episodes?preview=true  gets all episodes
 //https://api.simplecast.com/seasons/a9ec7b7a-02be-4252-9760-fab7c9945b7e   - get season
 //https://api.simplecast.com/episodes/38dd12b5-0197-49ee-b231-4a933609fd40?preview=true   - get episdode by ID
