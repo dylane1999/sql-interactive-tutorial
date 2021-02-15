3d34912f2ae4acca9f171ed486cff495c695383f
dan murphy <danielmurph8@gmail.com>
2021-02-14 20:47:30 -0500

    add space between node package imports and interal import statements, add morgan for better console logging

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
-// !use "npm run server" to start
+//! use "npm run server" to start
+
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
 import "./models/User.js";
 //* ^^^ This is importing a mongoDB schema.
@@ -6,15 +11,12 @@ import "./models/User.js";
 // or it will be duplicated in our DB.
 // to have access to the User model use:
 // const User = mongoose.model("User");
-
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
 import episodeRouter from "./routes/episodeRoutes.js";
 import testRouter from "./routes/route2.js";
 import userRouter from "./routes/userRouter.js";
 import connectDB from "./config/db.js";
 import seasonRouter from "./routes/seasonRoutes.js"
+import analyticsRouter from "./routes/analyticsRoutes.js";
 
 connectDB();
 
@@ -24,11 +26,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
+app.use(morgan("common"));
 
 app.use("/episodes", episodeRouter);
 app.use(seasonRouter);
 app.use(testRouter);
 app.use("/users", userRouter);
+app.use("/analytics", analyticsRouter);
 
 //const PORT = config.get("PORT");
 var PORT = process.env.PORT || 5000;
