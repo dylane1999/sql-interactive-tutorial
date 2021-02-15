e0a863c637f0f9dd20a2ce81a6406ef87223d63a
dan murphy <danielmurph8@gmail.com>
2021-02-14 19:28:17 -0500

    move analytics routes to individual file

---
+++ server/routes/analyticsRoutes.js
@@ -0,0 +1,35 @@
+import express from "express";
+import axios from "axios";
+import config from "config";
+
+const SIMPLECAST_BEARER = config.get("SIMPLECAST_BEARER");
+const router = express.Router();
+
+router.get("/top_10", async (req, res) => {
+    /**
+     * Gets the top 10 episodes for a specific podcast (based off ``podcastId``).
+     *
+     * @param req -> the request object which contains the body and params (``podcastId``) of the request
+     * @param res -> the response object, which is returned with the correct status header and top ten episodes data
+     *
+     * @return -> res.status(200).send(episodeData) || res.status(400).json(error)
+     */
+    try {
+      const podcastId = config.get("PODCAST_ID");
+      const topEpisodeData = await axios.get(
+        `https://api.simplecast.com/analytics/episodes/top_10?podcast=${podcastId}`,
+        {
+          headers: {
+            authorization: SIMPLECAST_BEARER,
+          },
+        }
+      );
+      console.log(topEpisodeData);
+      res.status(200).send(topEpisodeData.data);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ error: error });
+    }
+  });
+  
+  export default router;
