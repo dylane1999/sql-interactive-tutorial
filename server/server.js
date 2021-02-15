e36da404de9d13049b14b13647af53c711bf29ee
dan murphy <danielmurph8@gmail.com>
2021-02-14 19:26:56 -0500

    add /episodes prefix to each episode route

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
 
-app.use(episodeRouter);
+app.use("/episodes", episodeRouter);
 app.use(seasonRouter);
 app.use(testRouter);
 app.use("/users", userRouter);
