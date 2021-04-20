aae179dd14f668d398b916eec9bee8932846d3db
Michael Phaneuf <mdphaneuf@yahoo.com>
2021-04-19 21:21:51 -0700

    add button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,21 +2,26 @@ import React from "react";
 import incrementValue from "./middleware/Counter/incrementValue";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./store";
+import InteractiveSqlPage from "./views/pages/InteractiveSqlPage"
+
+let isCorrect = false
 
 function App() {
   const dispatch = useDispatch();
   const state = useSelector((state: RootState) => state.counter.currentValue);
   /** Alternate: const state = useSelector<RootState, number>(state => state.counter.currentValue); */
+  function setCorrect(value: boolean){
+    isCorrect = value
+    console.log(isCorrect)
+  }
   return (
     <>
-      <div className="App"> {state}</div>
-      <button
-        onClick={() => {
-          dispatch(incrementValue(1));
-        }}
-      >
-        click to increment
-      </button>
+    {!isCorrect === true
+      ?<div className="App">
+        <InteractiveSqlPage isCorrect={isCorrect} setCorrect={setCorrect}/>
+      </div>
+      : <></>
+    }
     </>
   );
 }
--- client/src/views/pages/InteractiveSqlPage.tsx
+++ client/src/views/pages/InteractiveSqlPage.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import EditorMenu, { IEditorMenuProps } from "../components/EditorMenu";
 import Instructions, {IInstructionsProps } from "../components/Instructions"
+import InteractiveQuery, {IInteractiveQueryProps} from "../components/InteractiveQuery/index"
+import { Button, makeStyles } from "@material-ui/core";
 
 export interface IInteractiveSqlPageProps {
-    x: number
+  isCorrect: boolean,
+  setCorrect: any
 }
 
 const Root = styled.div`
@@ -15,7 +18,42 @@ const Root = styled.div`
 `;
 
 const InteractiveSqlPage = (props: IInteractiveSqlPageProps) => {
-  return <Root></Root>;
+  const [CurrentQuery, setCurrentQuery] = useState<string>("");
+
+  function setInstructionsText(){
+    return "Select all of the People from the People table"
+  }
+
+  function setInstructionsTitle(){
+    return "Challenge #1"
+  }
+
+  let expectedQuery = "SELECT * FROM People"
+
+  function handleClick(){
+    props.setCorrect(false)
+    console.log(props.isCorrect)
+    setInstructionsText()
+  }
+
+  useEffect(() => {
+    if (CurrentQuery === expectedQuery) {
+      props.setCorrect(true)
+    }
+    console.log(props.isCorrect)
+    props.setCorrect(!props.isCorrect)
+  })
+
+  return (
+  <Root>
+    {!props.isCorrect
+    ? <Instructions instructionsText={setInstructionsText()} instructionsTitle={setInstructionsTitle()} isCorrect={props.isCorrect}/>
+    : <Button onClick={handleClick}>Next Challenge</Button>
+    } 
+    <InteractiveQuery currentQuery={setCurrentQuery}/>
+    <button onClick={()=> console.log(props.isCorrect)}>Check</button>
+  </Root>
+  );
 };
 
 export default InteractiveSqlPage;
